b23ca19274d82894c682c8000b497f67b9cc164f
Matthew Samushonok <matthew.samushonok@gmail.com>
2019-12-22 01:12:04 +0200

    Adding styles to app

--- src/main/java/com/example/iotimList/Application.java
+++ src/main/java/com/example/iotimList/Application.java
@@ -1,12 +1,21 @@
 package com.example.iotimList;
 
 import com.example.iotimList.domain.Task;
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static com.samskivert.mustache.Mustache.*;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public Mustache.Compiler mustacheCompiler(TemplateLoader loader) {
+        return compiler().escapeHTML(false).withLoader(loader);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
